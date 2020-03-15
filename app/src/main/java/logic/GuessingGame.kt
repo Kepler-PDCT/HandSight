e5feb7f3444136ecaed70e1c722839195c427b3f
unknown <jabos123@gmail.com>
2020-03-15 19:04:21 +0100

    Formats GuessingGame.

--- app/src/main/java/logic/GuessingGame.kt
+++ app/src/main/java/logic/GuessingGame.kt
@@ -1,6 +1,6 @@
 package logic
 
-class GuessingGame: Game<Char>() {
+class GuessingGame : Game<Char>() {
     override fun nextQuestion(): Question<Char> {
         var questionArray = (0..3).map { ((0..25).random() + 65).toChar() }.toList()
         val right = questionArray[(0..3).random()]
@@ -10,7 +10,7 @@ class GuessingGame: Game<Char>() {
     override fun makeGuess(guess: Char): Boolean {
         assert(!finished) // Should not be called if game is finished.
         val isCorrect = getQuestion().correctAnswer == guess
-        if(isCorrect) {
+        if (isCorrect) {
             updateScore(1)
         }
         updateCounter()
