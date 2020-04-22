91d52f03925bc261d5bbef3fdee173feacdbfa51
zimonitrome <zimonitrome@gmail.com>
2020-04-22 12:45:36 +0200

    Restores debug parameter.

--- app/src/main/java/logic/ImitationChallengeGame.kt
+++ app/src/main/java/logic/ImitationChallengeGame.kt
@@ -3,7 +3,7 @@ package logic
 class ImitationChallengeGame : Game<Char>(10) {
     override fun nextQuestion(): Question<Char> {
         var questionArray = (0..3).map { ((0..25).random() + 65).toChar() }.toList()
-        val right = 'B' //questionArray[(0..3).random()]
+        val right = questionArray[(0..3).random()]
         return Question(right, questionArray)
     }
 
