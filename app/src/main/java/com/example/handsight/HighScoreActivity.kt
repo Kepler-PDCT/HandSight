b1eeba2cda1392270ea10f2d63dec68b0ea9ed55
Filip Mattsson <famattsson@gmail.com>
2020-05-12 10:00:42 +0200

    Adds highscore view.

---
+++ app/src/main/java/com/example/handsight/HighScoreActivity.kt
@@ -0,0 +1,12 @@
+package com.example.handsight
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class HighScoreActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_high_score)
+    }
+}
