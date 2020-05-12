deab3ea72d30cc12252da712bdef3b492b335da5
Filip Mattsson <famattsson@gmail.com>
2020-05-12 10:31:14 +0200

    Merges

--- app/src/main/java/com/example/handsight/HighScoreActivity.kt
+++ app/src/main/java/com/example/handsight/HighScoreActivity.kt
@@ -2,11 +2,16 @@ package com.example.handsight
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_high_score.*
 
 class HighScoreActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_high_score)
+        GuessingModeText.text = "Guessing Mode:" + "" //get from local storage
+        ImitationModeText.text = "Imitation Mode" + ""
+        ChallengeModeText.text = "Challenge Mode:" + ""
+        WordModeText.text = "Word Mode:" + ""
     }
 }
